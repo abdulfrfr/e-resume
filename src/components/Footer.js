2e7dc2fb11616fc24e9ad23fc87168c0541a3757
abdulfrfr <abdulmails112@gmail.com>
2022-12-16 13:04:02 +0100

    'updates'

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer(){
         <section className="mt-10 w-[50vw] mx-auto">
 
         <div>
-            <p className='text-gray-700 text-md flex justify-center items-center'><BiCopyright className='text-lg mr-1'/> 2022 @abdulfrfr </p>
+            <p className='text-gray-700 text-sm md:text-md flex justify-center items-center'><BiCopyright className='text-lg mr-1'/> 2022 @abdulfrfr </p>
         </div>
 
         </section>
